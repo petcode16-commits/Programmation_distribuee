5734c33bd1985a88664a660f2ff11a9e2a204c33
petcode16-commits <petcode16@gmail.com>
2026-02-06 02:37:18 +0000

    v5

--- chauffage.c
+++ chauffage.c
@@ -3,70 +3,133 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/select.h>
 #include "msg_temperature.h"
 
+#define MAX_BUF 256
+
 int main(int argc, char *argv[]) {
+    // Correction du warning unused parameter 'argc' par une vérification de sécurité
+    if (argc != 6) {
+        fprintf(stderr, "Usage: %s <addr_multi> <port_multi> <nom_piece> <host_central> <port_central>\n", argv[0]);
+        exit(1);
+    }
+
+    char *addr_multi = argv[1];
+    int port_multi = atoi(argv[2]);
     char *nom_piece = argv[3];
-    int sock_tcp = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr_c;
-    addr_c.sin_family = AF_INET;
-    addr_c.sin_port = htons(atoi(argv[5]));
-    inet_pton(AF_INET, argv[4], &addr_c.sin_addr);
-    connect(sock_tcp, (struct sockaddr *)&addr_c, sizeof(addr_c));
+    char *host_central = argv[4];
+    int port_central = atoi(argv[5]);
 
-    dprintf(sock_tcp, "CHAUFFAGE %s\n", nom_piece);
+    int puissance = 0;       
+    int temp_cible = 20;     
+    int mode_auto = 0;       
 
+    // --- 1. SETUP MULTICAST (Écoute de l'Air) ---
     int sock_m = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in addr_m = {0};
+    int reuse = 1;
+    setsockopt(sock_m, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    struct sockaddr_in addr_m;
+    memset(&addr_m, 0, sizeof(addr_m)); // Nettoyage structure
     addr_m.sin_family = AF_INET;
-    addr_m.sin_port = htons(atoi(argv[2]));
+    addr_m.sin_port = htons(port_multi);
     addr_m.sin_addr.s_addr = INADDR_ANY;
     bind(sock_m, (struct sockaddr *)&addr_m, sizeof(addr_m));
 
     struct ip_mreq mreq;
-    mreq.imr_multiaddr.s_addr = inet_addr(argv[1]);
+    mreq.imr_multiaddr.s_addr = inet_addr(addr_multi);
     mreq.imr_interface.s_addr = htonl(INADDR_ANY);
     setsockopt(sock_m, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
 
-    int puissance = 0, mode_auto = 0, cible = 20;
-    fd_set rfds;
+    // --- 2. SETUP TCP (Connexion au Central) ---
+    int sock_tcp = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr_c;
+    memset(&addr_c, 0, sizeof(addr_c)); // Nettoyage structure
+    addr_c.sin_family = AF_INET;
+    addr_c.sin_port = htons(port_central);
+    inet_pton(AF_INET, host_central, &addr_c.sin_addr);
 
-    while(1) {
-        FD_ZERO(&rfds);
-        FD_SET(sock_tcp, &rfds);
-        FD_SET(sock_m, &rfds);
-        select(FD_SETSIZE, &rfds, NULL, NULL, NULL);
+    if (connect(sock_tcp, (struct sockaddr *)&addr_c, sizeof(addr_c)) < 0) {
+        perror("Échec connexion Central");
+        exit(1);
+    }
 
-        if (FD_ISSET(sock_tcp, &rfds)) {
-            char b[256]; int n = read(sock_tcp, b, 255);
-            if (n <= 0) break;
-            b[n] = '\0';
-            if (sscanf(b, "SET %*s %d", &puissance) == 1) mode_auto = 0;
-            if (sscanf(b, "AUTO %*s %d", &cible) == 1) mode_auto = 1;
-            dprintf(sock_tcp, "STAT %s %s %d\n", nom_piece, mode_auto?"AUTO":"MANU", puissance);
-        }
+    // Identification auprès du Central
+    dprintf(sock_tcp, "CHAUFFAGE %s\n", nom_piece);
+    printf("[CHAUFFAGE] Module %s connecté au Central.\n", nom_piece);
 
-        if (FD_ISSET(sock_m, &rfds)) {
-            unsigned char bm[256];
-            int n = recv(sock_m, bm, 256, 0);
+    fd_set readfds;
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(sock_m, &readfds);
+        FD_SET(sock_tcp, &readfds);
+        int max_fd = (sock_m > sock_tcp) ? sock_m : sock_tcp;
+
+        if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0) continue;
+
+        // --- RÉCEPTION BINAIRE DEPUIS L'AIR (MULTICAST) ---
+        if (FD_ISSET(sock_m, &readfds)) {
+            unsigned char buf_m[MAX_BUF];
+            int n = recv(sock_m, buf_m, sizeof(buf_m), 0);
             struct msg_temperature msg;
-            if (mt_parse(bm, n, &msg) == 0) {
-                if (msg.type == MT_MESURE && mode_auto) {
-                    puissance = (msg.valeur < cible) ? 3 : 0;
+            
+            if (mt_parse(buf_m, n, &msg) == 0) {
+                // Si c'est une mesure de notre pièce et qu'on est en mode AUTO
+                if (msg.type == MT_MESURE && strcmp(msg.piece, nom_piece) == 0 && mode_auto) {
+                    int ancienne_pwr = puissance;
+                    puissance = (msg.valeur < temp_cible) ? 3 : 0;
+
+                    if (puissance != ancienne_pwr) {
+                        dprintf(sock_tcp, "STAT %s AUTO_%s %d\n", nom_piece, (puissance>0?"ON":"OFF"), puissance);
+                    }
                 }
                 mt_free(&msg);
             }
-            // Envoi de la puissance à l'Air
+
+            // Envoi de la puissance actuelle vers l'Air (Format Binaire)
             struct msg_temperature out = {puissance, MT_CHAUFFER, nom_piece};
             size_t out_len;
             unsigned char *out_buf = mt_serialize(&out, &out_len);
-            struct sockaddr_in dest = {0};
-            dest.sin_family = AF_INET;
-            dest.sin_port = htons(atoi(argv[2]));
-            dest.sin_addr.s_addr = inet_addr(argv[1]);
-            sendto(sock_m, out_buf, out_len, 0, (struct sockaddr*)&dest, sizeof(dest));
+            
+            struct sockaddr_in dest_m;
+            memset(&dest_m, 0, sizeof(dest_m));
+            dest_m.sin_family = AF_INET;
+            dest_m.sin_port = htons(port_multi);
+            dest_m.sin_addr.s_addr = inet_addr(addr_multi);
+            
+            sendto(sock_m, out_buf, out_len, 0, (struct sockaddr*)&dest_m, sizeof(dest_m));
             free(out_buf);
         }
+
+        // --- RÉCEPTION COMMANDES DEPUIS LE CENTRAL (TCP) ---
+        if (FD_ISSET(sock_tcp, &readfds)) {
+            char b[MAX_BUF];
+            int n = read(sock_tcp, b, MAX_BUF - 1);
+            if (n <= 0) {
+                printf("[INFO] Déconnexion du Central. Fin du module.\n");
+                break;
+            }
+            b[n] = '\0';
+
+            int val;
+            if (sscanf(b, "SET %*s %d", &val) == 1) {
+                mode_auto = 0;
+                puissance = val;
+                printf("[%s] Mode MANU: Puissance %d\n", nom_piece, puissance);
+                dprintf(sock_tcp, "STAT %s MANU %d\n", nom_piece, puissance);
+            } 
+            else if (sscanf(b, "AUTO %*s %d", &val) == 1) {
+                mode_auto = 1;
+                temp_cible = val;
+                printf("[%s] Mode AUTO: Cible %d°C\n", nom_piece, temp_cible);
+                dprintf(sock_tcp, "STAT %s AUTO_START %d\n", nom_piece, puissance);
+            }
+        }
     }
+
+    close(sock_tcp);
+    close(sock_m);
     return 0;
 }
